56ed9465fd3abfdd38e53d4669b628974fbde066
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-08-12 03:25:54 +0300

    Updates

---
+++ precache-manifest.03fd97586a7155a9180afdb7ac38834d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843575792e04ac5c663466e6f7af734f",
+    "url": "/gitapp/index.html"
+  },
+  {
+    "revision": "c304fb3b8d6c4d717fe5",
+    "url": "/gitapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83625a777cb5af5b9703",
+    "url": "/gitapp/static/js/2.64bb6107.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitapp/static/js/2.64bb6107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c304fb3b8d6c4d717fe5",
+    "url": "/gitapp/static/js/main.2cfdc378.chunk.js"
+  },
+  {
+    "revision": "c98e2e261d5371c538fb",
+    "url": "/gitapp/static/js/runtime-main.5b3fca56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
